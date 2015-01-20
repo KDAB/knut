1e9e0927667b4044ab0dcbbce0892ff52e565c9a
Rafael Roquetto <rafael.roquetto@kdab.com>
2015-01-20 09:16:44 -0200

    Initial Qt5 import.
    Change-Id: I8e751e9fee63b160fa7452ee0d613c485256dbab

---
+++ main.cpp
@@ -0,0 +1,59 @@
+/**********************************************************************
+**
+** Copyright (C) 2005-2007 Trolltech ASA.  All rights reserved.
+**
+** This file is part of Qt Designer.
+**
+** This file may be distributed and/or modified under the terms of the
+** GNU General Public License version 2 as published by the Free Software
+** Foundation and appearing in the file LICENSE.GPL included in the
+** packaging of this file.
+**
+** Licensees holding valid Qt Enterprise Edition or Qt Professional Edition
+** licenses may use this file in accordance with the Qt Commercial License
+** Agreement provided with the Software.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+** See http://www.trolltech.com/gpl/ for GPL licensing information.
+** See http://www.trolltech.com/pricing.html or email sales@trolltech.com for
+**   information about Qt Commercial License Agreements.
+**
+** Contact info@trolltech.com if any conditions of this licensing are
+** not clear to you.
+**
+**********************************************************************/
+
+#include <QCoreApplication>
+#include <QStringList>
+#include <QString>
+#include <QFile>
+
+#include "rc2ui.h"
+
+static QStringList import(const QString &filename)
+{
+    QFile file(filename);
+
+    if (!file.open(QIODevice::ReadOnly))
+        qWarning("Could not open file '%s' ", filename.toLatin1().constData());
+
+    QTextStream in(&file);
+
+    RC2UI c(&in);
+    QStringList files;
+    c.parse();
+    return c.targetFiles;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    import(app.arguments().at(1));
+
+    return 0;
+}
+
+
---
+++ rc.pro
@@ -0,0 +1,5 @@
+TEMPLATE	= app
+CONFIG		+= qt warn_on release plugin
+HEADERS		= rc2ui.h
+SOURCES		= main.cpp rc2ui.cpp
+TARGET		= rcplugin
