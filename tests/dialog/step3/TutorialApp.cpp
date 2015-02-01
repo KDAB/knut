0d4147d8e0700d347dcebeca86e7dd1af13f341c
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2015-02-01 11:29:06 +0100

    dialog porting step3: port all code
    Change-Id: Iee3347bae7b8084556cd5ae60fc17cb4e31c999a

---
+++ tests/dialog/step3/TutorialApp.cpp
@@ -0,0 +1,48 @@
+#include "stdafx.h"
+#include "TutorialApp.h"
+#include "TutorialDlg.h"
+
+#include <qmfcapp.h>
+#include <qwinwidget.h>
+
+// DEBUG_NEW macro allows MFC applications to determine memory leak locations in debug builds
+#ifdef _DEBUG
+  #define new DEBUG_NEW
+#endif
+
+
+BEGIN_MESSAGE_MAP(CTutorialApp, CWinApp)
+END_MESSAGE_MAP()
+
+CTutorialApp::CTutorialApp()
+{
+}
+
+// The one and only CTutorialApp object
+CTutorialApp theApp;
+
+BOOL CTutorialApp::InitInstance()
+{
+  // InitCommonControls() is required on Windows XP if an application
+  // manifest specifies use of ComCtl32.dll version 6 or later to enable
+  // visual styles.  Otherwise, any window creation will fail.
+  InitCommonControls();
+
+  // Qt initialization
+  QMfcApp::instance(this);
+
+  // Create and show our dialog
+  CTutorialDlg dlg;
+  dlg.exec();
+
+  // Since the dialog has been closed, return FALSE so that we exit the
+  //  application, rather than start the application's message pump.
+  return false;
+}
+
+BOOL CTutorialApp::Run()
+{
+    int result = QMfcApp::run(this);
+    delete qApp;
+    return result;
+}
