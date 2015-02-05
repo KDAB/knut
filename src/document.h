501092cce519df6d7e3e04b54b90d68707f17a25
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2015-02-05 21:24:25 +0100

    Unifying parser result into a QJsonBoject
    Will be used as a basis for the command line interface
    
    Change-Id: I3f57a51352a65dc3159f25456307ae183933ed39

--- src/document.h
+++ src/document.h
@@ -1,7 +1,7 @@
 #ifndef DOCUMENT_H
 #define DOCUMENT_H
 
-#include <QJsonDocument>
+#include <QJsonObject>
 
 class Document
 {
@@ -9,12 +9,15 @@ public:
     Document(const QString &fileName);
     ~Document();
 
+    QString getAsset(const QString &id);
+    QJsonObject getDialog(const QString &id);
+
 private:
     void parse(const QString &fileName);
 
 private:
     // List of dialogs
-    QJsonDocument m_dialogs;
+    QJsonObject m_data;
 };
 
 #endif // DOCUMENT_H
