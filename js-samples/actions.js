e1b56f887cc1f989b7d151b82811d031f84dc3f3
Laurent Montel <laurent.montel@kdab.com>
2019-05-15 17:59:23 +0200

    Add js scripts in git directly
    Change-Id: I7d7c604fb3aba6575dc9c67b3605219f370fe1bf

---
+++ js-samples/actions.js
@@ -0,0 +1,17 @@
+function main() {
+	var doc = result.createDocument("CreateActions")
+	
+    doc.content = "QAction *action = nullptr;\n\n"
+	for (var i = 0; i < actions.length; ++i) {
+		var action = actions[i]
+        doc.content += qsTr("action = m_actionManager->CreateAction(%1, tr(\"%2\"), this);\n").arg(action.id).arg(action.title)
+		if (action.checkable) {
+			doc.content += "action->setCheckable(true);\n"
+			doc.content += "action->setChecked(true);\n"
+		}
+        if (action.toolTip !== "")
+            doc.content += qsTr("action->setToolTip(tr(\"%1\"));\n").arg(action.toolTip)
+        if (action.statusTip !== "")
+            doc.content += qsTr("action->setStatusTip(tr(\"%1\"));\n").arg(action.statusTip)
+	}
+}
