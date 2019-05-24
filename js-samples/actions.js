b38f810624aa58d1a06f6a92e708fe9846b39cd8
Laurent Montel <laurent.montel@kdab.com>
2019-05-24 12:18:18 +0200

    Allow to extract shortcut too
    Change-Id: I1087157fca9c95d3515b5a4bf3efeaee015c9188

--- js-samples/actions.js
+++ js-samples/actions.js
@@ -13,5 +13,21 @@ function main() {
             doc.content += qsTr("action->setToolTip(tr(\"%1\"));\n").arg(action.toolTip)
         if (action.statusTip !== "")
             doc.content += qsTr("action->setStatusTip(tr(\"%1\"));\n").arg(action.statusTip)
+        if (action.shortcuts.length === 1) {
+            doc.content += qsTr("action->setShortcut(QKeySequence(QStringLiteral(\"%1\")));\n").arg(action.shortcuts[0].event)
+        } else if (action.shortcuts.length >= 1) {
+            doc.content += "{\n";
+            doc.content += "    const QList< QKeySequence > lstShortcuts {";
+            for (var short = 0; short < action.shortcuts.length; short++) {
+                if (short > 0) {
+                    doc.content += ", ";
+                }
+
+                doc.content += qsTr("QKeySequence(QStringLiteral(\"%1\"))").arg(action.shortcuts[short].event)
+            }
+            doc.content += "};\n";
+            doc.content += "    action->setShortcuts(lstShortcuts);\n"
+            doc.content += "}\n";
+        }
 	}
 }
