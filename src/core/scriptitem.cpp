a9eb620b9468eb2b2adfbe41f5689b85b3f496d2
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2021-04-07 19:35:02 +0200

    Add ScriptItem object
    This object will be used as basis for all scriptable component you can create in QML.
    It allows to have childrean (contrary to QtObject).
    
    Change-Id: Idbfcda32dd4ddc87a1c8878fa91207346df674d9

---
+++ src/core/scriptitem.cpp
@@ -0,0 +1,68 @@
+#include "scriptitem.h"
+
+namespace Core {
+
+/*!
+ * \qmltype Script
+ * \brief Provides a script object for writing non visual scripts.
+ * \instantiates Core::ScriptItem
+ * \inqmlmodule Script
+ * \since Script 1.0
+ *
+ * The Script is the base class for all creatable items in QML. It is needed as a QtObject
+ * can't have any children in QML. It can be used as the basis for non visual QML scripts:
+ *
+ * \code
+ * import Script 1.0
+ *
+ * Script {
+ * // ...
+ * }
+ * \endcode
+ */
+
+ScriptItem::ScriptItem(QObject *parent)
+    : QObject(parent)
+{
+}
+
+ScriptItem::~ScriptItem() { }
+
+QQmlListProperty<QObject> ScriptItem::data()
+{
+    return QQmlListProperty<QObject>(this, nullptr, &ScriptItem::appendData, &ScriptItem::countData,
+                                     &ScriptItem::atData, &ScriptItem::clearData);
+}
+
+void ScriptItem::appendData(QQmlListProperty<QObject> *list, QObject *obj)
+{
+    if (auto that = qobject_cast<ScriptItem *>(list->object)) {
+        obj->setParent(that);
+        that->m_data.push_back(obj);
+        emit that->dataChanged();
+    }
+}
+
+QObject *ScriptItem::atData(QQmlListProperty<QObject> *list, int index)
+{
+    if (auto that = qobject_cast<ScriptItem *>(list->object))
+        return that->m_data.at(index);
+    return nullptr;
+}
+
+int ScriptItem::countData(QQmlListProperty<QObject> *list)
+{
+    if (auto that = qobject_cast<ScriptItem *>(list->object))
+        return static_cast<int>(that->m_data.size());
+    return 0;
+}
+
+void ScriptItem::clearData(QQmlListProperty<QObject> *list)
+{
+    if (auto that = qobject_cast<ScriptItem *>(list->object)) {
+        that->m_data.clear();
+        emit that->dataChanged();
+    }
+}
+
+}
---
+++ src/core/scriptitem.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <QObject>
+#include <QQmlListProperty>
+
+#include <vector>
+
+namespace Core {
+
+class ScriptItem : public QObject
+{
+    Q_OBJECT
+
+    Q_PROPERTY(QQmlListProperty<QObject> data READ data NOTIFY dataChanged FINAL)
+    Q_CLASSINFO("DefaultProperty", "data")
+
+public:
+    explicit ScriptItem(QObject *parent = nullptr);
+    ~ScriptItem() override;
+
+    QQmlListProperty<QObject> data();
+
+signals:
+    void dataChanged();
+
+private:
+    static void appendData(QQmlListProperty<QObject> *list, QObject *obj);
+    static QObject *atData(QQmlListProperty<QObject> *list, int index);
+    static int countData(QQmlListProperty<QObject> *list);
+    static void clearData(QQmlListProperty<QObject> *list);
+
+private:
+    std::vector<QObject *> m_data;
+};
+
+}
+
+Q_DECLARE_METATYPE(Core::ScriptItem *)
