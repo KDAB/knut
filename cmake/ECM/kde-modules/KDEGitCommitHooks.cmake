99ea70f067fe184f7601704e65dbc16e4fb4ed1e
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2022-02-24 22:00:43 +0100

    Stop ECM rewriting my pre-commit hook at every configure!
    Change-Id: I3a268ccb69b09b0cf39dec34d92bba17814ee6a5
    Reviewed-on: https://codereview.kdab.com/c/knut/+/108193
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Nicolas Arnaud-Cormos <nicolas@kdab.com>

--- cmake/ECM/kde-modules/KDEGitCommitHooks.cmake
+++ cmake/ECM/kde-modules/KDEGitCommitHooks.cmake
@@ -46,31 +46,4 @@ set(PRE_COMMIT_HOOK_UNIX "${CMAKE_CURRENT_LIST_DIR}/kde-git-commit-hooks/pre-com
 set(CLANG_FORMAT_UNIX "${CMAKE_CURRENT_LIST_DIR}/kde-git-commit-hooks/clang-format.sh")
 
 function(KDE_CONFIGURE_GIT_PRE_COMMIT_HOOK)
-    set(_oneValueArgs "")
-    set(_multiValueArgs CHECKS)
-    cmake_parse_arguments(ARG "" "${_oneValueArgs}" "${_multiValueArgs}" ${ARGN} )
-
-    if(NOT ARG_CHECKS)
-        message(FATAL_ERROR "No checks were specified")
-    endif()
-    set(GIT_DIR "${CMAKE_SOURCE_DIR}/.git")
-
-    # In case of tarballs there is no .git directory
-    if (IS_DIRECTORY ${GIT_DIR})
-        # The pre-commit hook is a bash script, consequently it won't work on non-unix platforms
-        # git on Windows provides its own bash
-        if (UNIX OR WIN32)
-            if(KDE_CLANG_FORMAT_EXECUTABLE)
-                list(FIND ARG_CHECKS "CLANG_FORMAT" _index)
-                if (${_index} GREATER -1)
-                    set(CLANG_FORMAT_SCRIPT "\"$(git rev-parse --git-common-dir)\"/hooks/scripts/clang-format.sh")
-                    configure_file(${CLANG_FORMAT_UNIX} "${GIT_DIR}/hooks/scripts/clang-format.sh" @ONLY)
-                endif()
-            else()
-                message(WARNING "No clang-format executable was found, skipping the formatting pre-commit hook")
-            endif()
-
-            configure_file(${PRE_COMMIT_HOOK_UNIX} "${GIT_DIR}/hooks/pre-commit")
-        endif()
-    endif()
 endfunction()
