4060d2cf4cc349f8afa0acb9a340e38eaa752bae
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2023-03-03 18:34:50 +0100

    Update nlohmann::json
    Change-Id: I83aa4d84cacc63255d724f43144fd27b85d5748a
    Reviewed-on: https://codereview.kdab.com/c/knut/+/122628
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Nicolas Arnaud-Cormos <nicolas@kdab.com>

--- src/core/json_utils.h
+++ src/core/json_utils.h
@@ -19,8 +19,9 @@ inline void from_json(const nlohmann::json &j, QString &str)
 {
     if (j.is_string())
         str = QString::fromStdString(j.get<std::string>());
-    else
-        throw nlohmann::detail::type_error::create(302, "type must be a string, but is not", j);
+    else {
+        throw nlohmann::detail::type_error::create(302, "type must be a string, but is not", &j);
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -38,7 +39,7 @@ inline void from_json(const nlohmann::json &j, QStringList &strList)
         auto list = j.get<std::vector<QString>>();
         strList = QStringList(list.cbegin(), list.cend());
     } else {
-        throw nlohmann::detail::type_error::create(302, "type must be an array, but is not", j);
+        throw nlohmann::detail::type_error::create(302, "type must be an array, but is not", &j);
     }
 }
 
@@ -68,7 +69,7 @@ struct adl_serializer<QFlags<T>>
             for (auto v : list)
                 data.setFlag(v);
         } else {
-            throw nlohmann::detail::type_error::create(302, "type must be an array, but is not", j);
+            throw nlohmann::detail::type_error::create(302, "type must be an array, but is not", &j);
         }
     }
 };
