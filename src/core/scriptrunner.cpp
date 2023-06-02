dab8a1b8484a3a98e29ead8cb469ea8079cbf2a6
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2023-06-02 19:43:28 +0200

    Fix binding loop with js scripts, in some cases
    Change-Id: Iccb18b9130c109cbd23197bbb386ef8af92ae415
    Reviewed-on: https://codereview.kdab.com/c/knut/+/126469
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Leon Matthes <leon.matthes@kdab.com>

--- src/core/scriptrunner.cpp
+++ src/core/scriptrunner.cpp
@@ -210,11 +210,13 @@ QQmlEngine *ScriptRunner::getEngine(const QString &fileName)
 
 QVariant ScriptRunner::runJavascript(const QString &fileName, QQmlEngine *engine)
 {
-    const QString text = QStringLiteral("import QtQml 2.12\n"
-                                        "import Script 1.0\n"
-                                        "import \"%1\" as MyScript\n"
-                                        "QtObject { property var result: MyScript.main() }")
-                             .arg(QUrl::fromLocalFile(fileName).toString());
+    const QString text =
+        QStringLiteral(
+            "import QtQml 2.12\n"
+            "import Script 1.0\n"
+            "import \"%1\" as MyScript\n"
+            "QtObject { property var _scriptResult; Component.onCompleted : _scriptResult = MyScript.main() }")
+            .arg(QUrl::fromLocalFile(fileName).toString());
 
     QQmlComponent component(engine);
     component.setData(text.toLatin1(), QUrl::fromLocalFile(fileName));
@@ -223,7 +225,7 @@ QVariant ScriptRunner::runJavascript(const QString &fileName, QQmlEngine *engine
     engine->deleteLater();
     m_hasError = component.isError();
     if (component.isReady() && !m_hasError)
-        return result->property("result");
+        return result->property("_scriptResult");
 
     filterErrors(component);
     return QVariant(ErrorCode);
