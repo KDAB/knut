41d5480f5db83e264b6f2b8cf6738b29152617cc
Leon Matthes <leon.matthes@kdab.com>
2023-10-11 13:38:22 +0200

    TreeSitter: Fix incorrectly displayed field names
    Also bumps three-sitter and tree-sitter-cpp versions.
    
    Change-Id: If0085408ff4f15e27df442ffce8809dcb77e841f
    Reviewed-on: https://codereview.kdab.com/c/knut/+/132078
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Nicolas Arnaud-Cormos <nicolas@kdab.com>

--- src/treesitter/node.cpp
+++ src/treesitter/node.cpp
@@ -32,18 +32,69 @@ Node Node::namedChild(uint32_t index) const
 
 QString Node::fieldNameForChild(const Node &child) const
 {
-    if (!child.isNamed()) {
-        return "";
+    // Implementation note:
+    //
+    // I would expect the code below to work:
+    //
+    // const auto index = children().indexOf(child);
+    // QString result;
+    // if (index != -1) {
+    //     auto name = ts_node_field_name_for_child(m_node, static_cast<uint32_t>(index));
+    //     if (name) {
+    //         result = name;
+    //     }
+    // } else {
+    //     spdlog::warn("Node::fieldNameForChild - given node is not a child!");
+    // }
+    //
+    // However, the code produces incorrect field names in a few cases.
+    // Most notably a parameter declaration like this:
+    //
+    // const Thing &test
+    //
+    // returns `type` as the field name of the `const` token, instead of `Thing`, which is incorrect.
+    // When querying for the `type: (_) @type`, we do actually find `Thing`, so the field name returned here
+    // wouldn't match what the query actually finds.
+    //
+    // When looking at the Tree-Sitter playground [^1], this works correctly.
+    // Investigating the playground [^2] of this shows that the playground uses
+    // the TreeCursor to get the field name, which seems to work correctly.
+    // Therefore the code below fixes the issue by using a TreeCursor as well.
+    //
+    // This issue has already been reported as:
+    // https://github.com/tree-sitter/tree-sitter/issues/1642
+    // and fixed in:
+    // https://github.com/tree-sitter/tree-sitter/pull/2104
+    //
+    // However, this has been merged a few hours **after** the release 0.20.8.
+    // So it should be fixed in 0.20.9, but for now, we need to use the TreeCursor API
+    // until a new tree-sitter release is available.
+    //
+    // [^1]: https://tree-sitter.github.io/tree-sitter/playground
+    // [^2]:
+    // https://github.com/tree-sitter/tree-sitter/blob/20924fa4cdeb10d82ac308481e39bf8519334e55/docs/assets/js/playground.js#L178C35-L178C35
+
+    QString result;
+    bool found = false;
+
+    TSTreeCursor cursor = ts_tree_cursor_new(m_node);
+
+    if (ts_tree_cursor_goto_first_child(&cursor)) {
+        do {
+            if (Node(ts_tree_cursor_current_node(&cursor)) == child) {
+                found = true;
+
+                if (const auto *name = ts_tree_cursor_current_field_name(&cursor)) {
+                    result = name;
+                }
+                break;
+            }
+        } while (ts_tree_cursor_goto_next_sibling(&cursor));
     }
 
-    const auto index = namedChildren().indexOf(child);
-    QString result;
-    if (index != -1) {
-        auto name = ts_node_field_name_for_child(m_node, static_cast<uint32_t>(index));
-        if (name) {
-            result = name;
-        }
-    } else {
+    ts_tree_cursor_delete(&cursor);
+
+    if (!found) {
         spdlog::warn("Node::fieldNameForChild - given node is not a child!");
     }
 
