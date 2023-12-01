f7f4f539da9deb84b7445ced6cc9317d6cfdbae9
Renato Araujo Oliveira Filho <renato.araujo@kdab.com>
2023-12-01 16:05:24 -0300

    photon-ts: Implement tree sitter convertion for photon
    Use tree sitter conversion for photon projects
    
    Change-Id: I15b30c18ad8abca4275a3204ad3f753e4b09aada
    Reviewed-on: https://codereview.kdab.com/c/knut/+/134587
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Leon Matthes <leon.matthes@kdab.com>

---
+++ tests/tst_photon_treesitter.cpp
@@ -0,0 +1,54 @@
+#include "core/cppdocument.h"
+#include "core/knutcore.h"
+#include "core/project.h"
+#include "core/scriptmanager.h"
+
+#include "common/test_utils.h"
+
+#include <QTemporaryFile>
+#include <QTest>
+
+class TestPhoton : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void initTestCase() { Q_INIT_RESOURCE(core); }
+
+    void conversion_data()
+    {
+        QTest::addColumn<QString>("source");
+
+        QTest::addRow("PgColors") << "PgColors.cpp";
+    }
+
+    void conversion()
+    {
+        QFETCH(QString, source);
+
+        Test::LogSilencer ls;
+
+        Core::KnutCore core;
+        Core::Project::instance()->setRoot(Test::testDataPath() + "/tst_photon_ts");
+
+        testSource(source);
+    }
+
+    void testSource(const QString &source)
+    {
+        Test::FileTester file(Test::testDataPath() + "/tst_photon/" + source);
+        qobject_cast<Core::CppDocument *>(Core::Project::instance()->open(file.fileName()));
+
+        const auto scriptPath = QCoreApplication::applicationDirPath() + "/scripts/photon-convert-file-ts.qml";
+
+        QBENCHMARK {
+            Core::ScriptManager::instance()->runScript(scriptPath, false, true);
+        }
+
+        QVERIFY(file.compare());
+        Core::Project::instance()->closeAll();
+    }
+};
+
+QTEST_MAIN(TestPhoton)
+#include "tst_photon_treesitter.moc"
