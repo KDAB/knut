ef5e96f95a2aa1b1b55071e9ebdba2a98bbe3b92
Michel Boyer de la Giroday <michel.giroday@kdab.com>
2024-03-22 10:39:52 +0100

    Knut - Add qt_fmt feature from KDToolBox
    Create a utils library that contains qt_fmt
    
    Task-Id: KNUT-130
    Change-Id: I1fd254e1a0109244520305ae104f9fcfe577dd48
    Reviewed-on: https://codereview.kdab.com/c/knut/+/139640
    Reviewed-by: Leon Matthes <leon.matthes@kdab.com>
    Tested-by: Continuous Integration <build@kdab.com>
    Reviewed-by: Nicolas Arnaud-Cormos <nicolas@kdab.com>

---
+++ tests/tst_qt_fmt_format.cpp
@@ -0,0 +1,112 @@
+/*
+  This file is part of KDToolBox.
+
+  SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+  SPDX-License-Identifier: MIT
+*/
+
+#include "../utils/qt_fmt_format.h"
+
+#include <QDebug>
+#include <QRegularExpression>
+#include <QTest>
+
+#include <QDateTime>
+#include <QRect>
+#include <QSize>
+#include <QUuid>
+
+class QtFmtFormatTest : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void testQtFmtFormat();
+};
+
+// Custom class streamable via QDebug
+class CustomClass
+{
+};
+QDebug operator<<(QDebug d, CustomClass)
+{
+    return d << "CustomClass()";
+}
+
+// Custom class streamable via fmt::formatBothFmtAndQDebugClass and QDebug
+class BothFmtAndQDebugClass
+{
+};
+template <>
+struct fmt::formatter<BothFmtAndQDebugClass, char>
+{
+    template <typename ParseContext>
+    constexpr auto parse(ParseContext &ctx)
+    {
+        auto it = ctx.begin();
+        auto end = ctx.end();
+        if (it != end && *it != '}')
+            throw fmt::format_error("Only {} is supported");
+        return it;
+    }
+    template <typename FormatContext>
+    auto format(const BothFmtAndQDebugClass &, FormatContext &ctx) const
+    {
+        return fmt::format_to(ctx.out(), "BothFmtAndQDebugClass via std");
+    }
+};
+
+QDebug operator<<(QDebug d, BothFmtAndQDebugClass)
+{
+    return d << "BothFmtAndQDebugClass via QDebug";
+}
+
+template <>
+struct Qt_fmt::exclude_from_qdebug_fmt<BothFmtAndQDebugClass> : fmt::true_type
+{
+};
+
+void QtFmtFormatTest::testQtFmtFormat()
+{
+    // This is a bit sketchy because it depends on QDebug's format. Better than nothing?
+    using namespace fmt::literals;
+
+    // Fundamental / Standard Library types supported by fmt::format _and_ QDebug;
+    // should all go through fmt::format
+    QCOMPARE(fmt::format("{}", 42), "42");
+    QCOMPARE(fmt::format("{}", 10.5), "10.5");
+    QCOMPARE(fmt::format("{}", "hello"), "hello");
+    QCOMPARE(fmt::format("{}", (const char *)"hello"), "hello");
+
+    QCOMPARE(fmt::format("{}", "hello"s), "hello");
+    QCOMPARE(fmt::format("{}", "hello"sv), "hello");
+
+    // Qt types
+    QCOMPARE(fmt::format("{}", QStringLiteral("hello")), "hello");
+    QCOMPARE(fmt::format("{}", QByteArray("hello")), "hello");
+    QCOMPARE(fmt::format("{}", QDateTime(QDate(2000, 2, 29), QTime(12, 12, 12), Qt::UTC)),
+             "QDateTime(2000-02-29 12:12:12.000 UTC Qt::UTC)");
+    QCOMPARE(fmt::format("{}", QUuid()), "QUuid({00000000-0000-0000-0000-000000000000})");
+    QCOMPARE(fmt::format("{}", QRect()), "QRect(0,0 0x0)");
+    QCOMPARE(fmt::format("{}", QSizeF()), "QSizeF(-1, -1)");
+
+    // Q_FLAG / Q_ENUM
+    QCOMPARE(fmt::format("{}", Qt::Alignment(Qt::AlignLeft | Qt::AlignVCenter)),
+             "QFlags<Qt::AlignmentFlag>(AlignLeading|AlignVCenter)");
+    QCOMPARE(fmt::format("{}", Qt::AlignLeft), "Qt::AlignLeft");
+
+    // User defined types
+    QCOMPARE(fmt::format("{}", CustomClass()), "CustomClass()");
+    QCOMPARE(fmt::format("{}", BothFmtAndQDebugClass()), "BothFmtAndQDebugClass via std");
+}
+
+static_assert(Qt_fmt::detail::IsFormattableViaQDebug<QString>);
+static_assert(Qt_fmt::detail::IsFormattableViaQDebug<QByteArray>);
+static_assert(Qt_fmt::detail::IsFormattableViaQDebug<QDateTime>);
+static_assert(Qt_fmt::detail::IsFormattableViaQDebug<QUuid>);
+static_assert(Qt_fmt::detail::IsFormattableViaQDebug<QRect>);
+
+QTEST_APPLESS_MAIN(QtFmtFormatTest)
+
+#include "tst_qt_fmt_format.moc"
---
+++ utils/qt_fmt_format.h
@@ -0,0 +1,52 @@
+/*
+  This file is part of KDToolBox.
+  SPDX-FileCopyrightText: 2024 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+  SPDX-License-Identifier: MIT
+*/
+#pragma once
+
+#include "qt_fmt_helpers.h"
+
+#include <QDebug>
+#include <fmt/format.h>
+
+template <typename T>
+struct fmt::formatter<T, char,
+                      std::void_t<std::enable_if_t<!Qt_fmt::exclude_from_qdebug_fmt<T>::value>,
+                                  decltype(std::declval<QDebug &>() << std::declval<const T &>())>>
+{
+    constexpr auto parse(fmt::format_parse_context &ctx)
+    {
+        auto it = ctx.begin();
+        auto end = ctx.end();
+        if (it != end && *it != '}')
+            throw fmt::format_error("Only {} is supported");
+        return it;
+    }
+
+    template <typename FormatContext>
+    auto format(const T &t, FormatContext &ctx) const
+    {
+        // This is really expensive (lots of allocations). Unfortunately
+        // there isn't something as easy to do that also performs better.
+        //
+        // * storing buffer+debug as thread_local has reentrancy issues.
+        //   Say you format an object of type A; this calls operator<<(QDebug d, A a).
+        //   Inside that, you call d << a.b to stream a subobject of type B.
+        //   operator<<(QDebug d, B b) might be implemented itself using fmt, e.g.
+        //   via std::format(~~~, b.c, b.d, b.e).
+        //   Now if any sub-object ends up being printed using *this*
+        //   streaming (via QDebug), it won't work.
+        //
+        // * Using QByteArray + QBuffer to avoid the final conversion
+        //   to UTF-8 doesn't really help, it'll replace it with a bunch
+        //   of transient small allocations (as QDebug will convert each
+        //   string streamed into it in UTF-8 and then append to its
+        //   internal buffer)
+
+        QString buffer;
+        QDebug debug(&buffer);
+        debug.noquote().nospace() << t;
+        return fmt::format_to(ctx.out(), "{}", buffer.toStdString());
+    }
+};
