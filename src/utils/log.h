ebb0cd95220acdd1db0fed576c11d3a780545c43
Nicolas Arnaud-Cormos <nicolas@kdab.com>
2024-06-07 14:22:22 +0200

    chore: remove std::format supports for spdlog
    Just use the fmt lib bundled with it.
    
    Change-Id: I83d9974ebe63e74dee947f7b7d6cabfb959c6c83

--- src/utils/log.h
+++ src/utils/log.h
@@ -10,10 +10,5 @@
 
 #pragma once
 
-#include <spdlog/spdlog.h>
-
-#if defined(SPDLOG_USE_STD_FORMAT)
-#include "qt_std_format.h"
-#else
 #include "qt_fmt_format.h"
-#endif
+#include <spdlog/spdlog.h>
