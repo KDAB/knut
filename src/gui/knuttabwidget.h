ad0bbd300a1f1690972d8cff9bbfcd1eeb833942
Javier O. Cordero Pérez <javier.cordero@kdab.com>
2025-03-03 11:55:58 -0400

    feat: automatically show and hide close buttons from tab bar
    This implementation shows and hides all close buttons simultaneously
    while mouse cursor hovers over them. A delay was added before close
    buttons are hidden again to save the user from a jarring experience
    when they off-shoot the mouse cursor reaching for the tab bar.
    
    Ideally we'd only display close buttons for the active tab and the tab
    currently being hovered. However, the list of tabs is stored in the
    protected side of QTabBar (qtabbar_p.h) and we can't make use of it
    without re-inventing the wheel.

---
+++ src/gui/knuttabwidget.h
@@ -0,0 +1,40 @@
+/*
+  This file is part of Knut.
+
+SPDX-FileCopyrightText: 2025 Klarälvdalens Datakonsult AB, a KDAB Group company <info@kdab.com>
+
+SPDX-License-Identifier: GPL-3.0-only
+
+Contact KDAB at <info@kdab.com> for commercial licensing options.
+*/
+
+#pragma once
+
+#include <QObject>
+#include <QTabBar>
+#include <QTabWidget>
+#include <QWidget>
+
+namespace Gui {
+
+class KnutTabBar : public QTabBar
+{
+    Q_OBJECT
+public:
+    KnutTabBar(QWidget *parent);
+
+protected:
+    bool event(QEvent *event);
+
+private:
+    QTimer *m_hideCloseTimer;
+};
+
+class KnutTabWidget : public QTabWidget
+{
+    Q_OBJECT
+public:
+    KnutTabWidget(QWidget *parent);
+};
+
+} // namespace Gui
